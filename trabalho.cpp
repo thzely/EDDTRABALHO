9d1d9899e7d9b79f29de3a06a66208b58bde3dde
Marianne Fisch <tthzely@gmail.com>
2025-11-03 19:42:36 -0300

    base do trabalho

---
+++ trabalho.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <iomanip>
+
+using namespace std;
+
+struct entrada{
+    int senha;
+    char prio;
+    int hh;
+    int mm;
+};
+
+int fila(queue<entrada> filaEntrada) {
+    entrada auxiliar;
+    cin >> auxiliar.senha;
+    cin >> auxiliar.prio;
+    cin >> auxiliar.hh;
+    cin >> auxiliar.mm;
+
+}
+
+int main(){
+
+    queue<entrada> fila;
+
+
+
+    return 0;
+}
