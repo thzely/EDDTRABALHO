fb905985299f22b6b9e4ec1ed6da6c3a52e3bbb6
Marianne Fisch <tthzely@gmail.com>
2025-11-08 11:07:56 -0300

    Atualizações do código.

--- trabalho.cpp
+++ trabalho.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+queue<entrada> filaV;
+queue<entrada> filaA;
+queue<entrada> filaD;
+queue<entrada> filaB;
+
 struct entrada{
     int senha;
     char prio;
@@ -11,19 +16,53 @@ struct entrada{
     int mm;
 };
 
-int fila(queue<entrada> filaEntrada) {
+void novo_paciente(){
     entrada auxiliar;
     cin >> auxiliar.senha;
     cin >> auxiliar.prio;
     cin >> auxiliar.hh;
     cin >> auxiliar.mm;
 
+    if (auxiliar.prio == 'V'){
+        filaV.push(auxiliar);
+    } else if (auxiliar.prio == 'A'){
+        filaA.push(auxiliar);
+    } else if (auxiliar.prio == 'D'){
+        filaD.push(auxiliar);
+    } else(auxiliar.prio == 'B');{
+        filaB.push(auxiliar);
+    }
 }
 
 int main(){
 
     queue<entrada> fila;
 
+    cout << "---- MENU ----" << endl;
+    cout << "-- TRIAGEM HOSPITALAR --" << endl;
+    cout << "Para inserir um novo paciente digite C" << endl;
+    cout << "Para realizar um atendimento digite A" << endl;
+    cout << "Para exibir as informações sobre fila digite D" << endl;
+    cout << "Para encerrar o sistema digite Q" << endl;
+
+    string opcao = "M";
+
+    while (opcao != "Q"){
+
+        cin >> opcao;
+
+        if(opcao == "C"){
+            novo_paciente();
+            break;
+        } else if(opcao == "A"){
+
+            break;
+        } else(opcao == "D");{
+
+            break;
+        }
+        
+    }
 
 
     return 0;
